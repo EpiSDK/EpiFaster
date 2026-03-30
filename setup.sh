681dce43350d5402d7bced9cdd2c64c3133771ab
lukas-sgx <lukas.soigneux@epitech.eu>
2026-03-30 10:10:17 +0200

    [+] Add setup for nix os

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
-#!/usr/bin/bash
-
-mkdir -p /usr/lib/epiclang/
-sudo cp -r ./plugins /usr/lib/epiclang/
-make install
-chmod +x epiclang
-sudo cp epiclang /usr/bin/epiclang
-echo "Installation complete. Plugins have been copied to /usr/lib/epiclang/plugins/"
+if [ ! -d /usr/lib/ ]
+then
+    ./nix/setup.sh
+else
+    # sudo mkdir -p /usr/lib/epiclang/
+    # sudo cp -r ./plugins /usr/lib/epiclang/
+    # make install
+    # chmod +x epiclang
+    # sudo cp epiclang /usr/bin/epiclang
+    echo "Installation complete. Plugins have been copied to /usr/lib/epiclang/plugins/"
+fi
