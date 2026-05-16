647eb91e3e2899b79d39ea061e2d091ee2013d74
lukas-sgx <lukas.soigneux@epitech.eu>
2026-05-16 20:29:16 +0200

    fix: change header & add checker

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
 /*
-** EPITECH PROJECT, 2026
-** ~/epitech/free/EpiFaster/src
+** LUKAS SOIGNEUX PROJECT, 2026
+** directory
 ** File description:
-** main.rs
+** filename
 */
 
 use std::env;
 
-mod options;
 mod arguments;
 mod build;
+mod options;
+mod coding_style;
 
 pub fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     let parameters = arguments::get_parameters(args);
 
+    coding_style::ast::checker();
     if parameters.build {
         build::compiler(parameters.files, parameters.supplement_args);
     }
