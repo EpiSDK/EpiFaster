e99f6521a2736fb4a9fb861f867bad363dfc8172
lukas-sgx <lukas.soigneux@epitech.eu>
2026-05-18 00:26:40 +0200

    feat: implement temp multi thread

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,19 @@ mod coding_style;
 pub fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     let parameters = arguments::get_parameters(args);
-
-    coding_style::ast::checker();
+    let files = parameters.files;
+    // let mut handles = Vec::new();
+        
+    for file in files.clone() {
+    //     handles.push(thread::spawn(move || {
+            coding_style::banana::checker(file);
+        // }));
+    }
+    // for handle in handles {
+    //     handle.join().expect("Error on thread");
+    // }
+    
     if parameters.build {
-        build::compiler(parameters.files, parameters.supplement_args);
+        build::compiler(files, parameters.supplement_args);
     }
 }
