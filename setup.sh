2b6888e7136d73e718a8d54370f01311652a3f9a
lukas-sgx <lukas.soigneux@epitech.eu>
2026-08-21 14:58:46 +0200

    fix: update plugin path

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
-mkdir -p /opt/lib/epiclang/
+#!/bin/env sh
+
+mkdir -p /opt/epiclang/lib/
 mkdir -p $HOME/.local/bin/
+
 cp -r ./plugins /opt/epiclang/lib/
+
 cargo build -r
 cp target/release/epiclang $HOME/.local/bin/epiclang
+
 echo "Installation complete. Plugins have been copied to /opt/epiclang/lib/plugins/"
